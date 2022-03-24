37ebe5d54ebe1e1ab5de6bccc7a1f209a590ab36
DistiD <distida@student.undip.ac.id>
2022-03-24 17:05:48 +0700

    added menu 1-3

--- program_atm.py
+++ program_atm.py
@@ -15,3 +15,35 @@ while True:
         if trial == 3:
             print("Error. Silakan ambil kartu dan coba lagi..")
             exit()
+while True:
+        print("Selamat datang di ATM Kelompok 32..")
+        print("\n1 - Cek Saldo\t2 - Debet\t3 - Simpan\t4 - Ganti Pin\t5 - Keluar")
+        selectmenu = int(input("\nSilahkan pilih menu (1-5): "))
+        if selectmenu == 1:
+            print("\nSaldo anda sekarang: Rp. " + str(atm.cekBalance()) + "\n")
+        elif selectmenu == 2:
+            nominal = float(input("Masukkan nominal debet: "))
+            verify_withdraw = input(
+                "Konfirmasi: Anda akan melakukan debet dengan nominal berikut ? y / n " + str(nominal) + " ")
+            if verify_withdraw == "y":
+                print("Saldo awal anda adalah: Rp. " +
+                      str(atm.cekBalance()) + "")
+                if nominal < atm.cekBalance():
+                    atm.withdrawBalance(nominal)
+                    print("Transaksi debet berhasil!")
+                    print("Saldo sisa sekarang: Rp. " +
+                          str(atm.cekBalance()) + "")
+                else:
+                    print("Maaf. Saldo anda tidak cukup untuk melakukan debet!")
+                    print("Silakan lakukan penambahan nominal saldo")
+            else:
+                break
+        elif selectmenu == 3:
+            nominal = float(input("Masukkan nominal penyimpanan: "))
+            verify_deposit = input(
+                "Konfirmasi: Anda akan melakukan penyimpanan dengan nominal berikut? y / n :" + str(nominal) + " ")
+            if verify_deposit == "y":
+                atm.depositBalance(nominal)
+                print("Saldo anda sekarang: Rp. " + str(atm.cekBalance()) + "")
+            else:
+                break
